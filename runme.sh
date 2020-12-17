36a4d5ab5f33fddf4f3c93862e4c147ca8cabb01
sixZhang <zy6lc@qq.com>
2020-12-17 18:47:22 +0800

    change runme

--- runme.sh
+++ runme.sh
@@ -1,4 +1,7 @@
-python3 -m venv my_venv
-source my_venv/bin/activate
+#!/bin/bash
+
+#python3 -m venv my_venv
+#source my_venv/bin/activate
 pip install -r requirements.txt
-jupyter notebook --notebook-dir .
+python -m ipykernel install --user --name=my_venv
+jupyter notebook --notebook-dir

f7c349d1db975782d505369c448fb5dbecec8623
sixZhang <zy6lc@qq.com>
2020-12-17 18:50:47 +0800

    del runme

--- runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#python3 -m venv my_venv
-#source my_venv/bin/activate
-pip install -r requirements.txt
-python -m ipykernel install --user --name=my_venv
-jupyter notebook --notebook-dir
